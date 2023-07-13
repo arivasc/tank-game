ff7a1553082bdc010c2af3a5998e0891ab877877
Anthony Rivas <arivasc@unsa.edu.pe>
2023-07-13 11:08:36 -0500

    Trabajando con dos ventanas.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //sf::Window my_Window(sf::VideoMode(640U,480U),"w",sf::Style::None)
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::Window juego(sf::VideoMode(1080, 720), "Tank-Game");
+    //cambiar el no título de la ventana
+    window.setTitle("Menu principal");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
+        sf::Event eJuego;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed){
                 window.close();
-        }
+                juego.close();
+            }
+                
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+            
+            
+            
+        }
     }
 
     return 0;
