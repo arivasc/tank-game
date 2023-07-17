917657eac85679c176a3ba2792449a69c961c7df
Anthony Rivas <arivasc@unsa.edu.pe>
2023-07-17 13:51:34 -0500

    cambiado el tamaño de la pantalla

--- sourceFiles/Game.cpp
+++ sourceFiles/Game.cpp
@@ -3,7 +3,7 @@
 //Private functions
 void Game::initWindow()
 {
-	this->window = new sf::RenderWindow(sf::VideoMode(1300, 1300), "Swaglords of Space - Game 3", sf::Style::Close | sf::Style::Titlebar);
+	this->window = new sf::RenderWindow(sf::VideoMode(1080, 720), "Tank - Game");
 	this->window->setFramerateLimit(144);
 	this->window->setVerticalSyncEnabled(false);
 }
