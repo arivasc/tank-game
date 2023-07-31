3cde67d4390c5e889baf0c64cafbc858f768daef
FloresPatwil <pfloresri@unsa.edu.pe>
2023-07-30 22:46:44 -0500

    Comentando Bullet.h

--- headerFiles/Bullet.h
+++ headerFiles/Bullet.h
@@ -8,21 +8,23 @@ class Bullet
 {
 private:
 
-	sf::Sprite shape;
+	sf::Sprite shape;   //Representar graficamente en pantalla
 
-	sf::Vector2f direction;
-	float movementSpeed;
-	int direc; 
+	sf::Vector2f direction;   //Tomamos un vector en 2d
+	float movementSpeed;	//decimal que determina la velocidad de movimiento
+	int direc; 		//nesecitamos un estado que determine la direccion
 public:
 	Bullet();
+	//Constructor sobrecargado que tomara la tex, posicion, velocidad, y la direc
 	Bullet(sf::Texture* texture, float pos_x, float pos_y, float dir_x, float dir_y, float movement_speed,int direc);
+	//Constructor virtual de la clase
 	virtual ~Bullet();
 
 	//Accessor
 	const sf::FloatRect getBounds() const;
 
-	void update();
-	void render(sf::RenderTarget* target);
+	void update();	//Actualiza la posicion de la bala
+	void render(sf::RenderTarget* target);	//dibujar por acciones
 };
 
 #endif // !BULLET_H
